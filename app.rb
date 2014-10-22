672ec2a03033003cd399972d5b2f54762432213d
empayne <e.payne@utoronto.ca>
2014-10-21 23:04:38 -0400

    Configure to suppress Sinatra error messages.

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 # app.rb
 require 'sinatra'
 
-class HelloWorldApp < Sinatra::Base
-	get '/' do
+class PersonalWebsite < Sinatra::Base
+	set :environment, :production
+	set :raise_errors, false
+	set :show_exceptions, false
+
+	get %r{.*} do		# Show same page for any get request (for now)
+		puts "in here"
 	  haml :index
 	end
+
+	error do
+		puts "An error occurred..."
+	end
+
 end
